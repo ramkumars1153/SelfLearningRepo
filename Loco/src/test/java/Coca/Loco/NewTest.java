f9188f141deb45ea2e6d385f34dd341d01a843b0
ramkumars1153 <77505586+ramkumars1153@users.noreply.github.com>
2021-03-27 11:40:31 +0530

    Update NewTest.java

--- Loco/src/test/java/Coca/Loco/NewTest.java
+++ Loco/src/test/java/Coca/Loco/NewTest.java
@@ -14,7 +14,7 @@ public class NewTest {
 	  options.addArguments("--start-maximized");
 	  WebDriver driver = new ChromeDriver(options);
 	  driver.get("https://www.flipkart.com/");
-	  System.out.println("Flipkart is open by the user for testing the app");
+	  System.out.println("Flipkart is open by the user for testing the app make your app building dream come true");
 	  
   }
 }
